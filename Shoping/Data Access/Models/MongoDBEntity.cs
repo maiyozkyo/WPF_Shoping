b7e7ed3bb762cdba475f3fc8e417b1b12fad4d59
NHBUU <ekkobuu@gmail.com>
2024-01-22 23:33:10 +0700

    create base entity

---
+++ Shoping/Data Access/Models/MongoDBEntity.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoping.Data_Access.Models
+{
+    public class MongoDBEntity
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        protected string _id;
+
+        public string RecID { get; set; }
+        public int Index { get; set; }
+
+        public MongoDBEntity()
+        {
+            RecID = Guid.NewGuid().ToString();
+        }
+    }
+}
