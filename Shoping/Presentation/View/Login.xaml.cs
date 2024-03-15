bcf0c3022cf634da7f2617d8e83b8c6948f8c980
NguyenQui9112003 <147489314+NguyenQui9112003@users.noreply.github.com>
2024-03-15 22:50:03 +0700

    test create branch

--- Shoping/Presentation/View/Login.xaml.cs
+++ Shoping/Presentation/View/Login.xaml.cs
@@ -1,24 +1,13 @@
 ﻿using Shoping.Presentation.View;
 using Shoping.Presentation.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Shoping.Presentation
 {
     /// <summary>
     /// Interaction logic for Login.xaml
     /// </summary>
+    /// qui
     public partial class Login : Window
     {
         public LoginViewModel LoginViewModel { get; set; }
@@ -37,7 +26,7 @@ namespace Shoping.Presentation
             registerWindow.Show();
         }
 
-        
+
 
         private async void LoginClick(object sender, RoutedEventArgs e)
         {
