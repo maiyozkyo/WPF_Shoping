ea07cebcb8e8d247ea20ffe18524dcb565e9d670
nhnquang21@clc.fitus.edu.vn <138848005+Quangdt21127150@users.noreply.github.com>
2024-03-23 22:58:02 +0700

    Add statistic model view

---
+++ Shoping/Presentation/View/Statistic.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Shoping.Presentation.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Shoping.Presentation
+{
+    public partial class Statistic : Window
+    {
+        public StatisticViewModel StatisticViewModel { get; set; }
+        public Statistic()
+        {
+            InitializeComponent();
+            StatisticViewModel = new StatisticViewModel();
+        }
+
+        private void StatisticCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(StatisticCombobox.SelectedItem.ToString() == "From date to date")
+            {
+                DatePicker.Visibility = Visibility.Visible;
+            } else
+            {
+                DatePicker.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private async void RevenueButton_Click(object sender, RoutedEventArgs e)
+        {
+            var startDate = DateTime.Today;
+            var endDate = DateTime.Today;
+            if(StartDate.SelectedDate != null && EndDate.SelectedDate != null)
+            {
+                startDate = (DateTime)StartDate.SelectedDate;
+                endDate = (DateTime)EndDate.SelectedDate;
+            }
+            var isSuccess = await StatisticViewModel.DrawRevenueAndProfitDiagram(StatisticCombobox.SelectedIndex.ToString(), startDate, endDate);
+        }
+    }
+}
---
+++ Shoping/Presentation/ViewModels/StatisticViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Shoping.Business.StatisticServices;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace Shoping.Presentation.ViewModels
+{
+    public class StatisticViewModel
+    {
+        IStatisticBusiness StatisticServices;
+
+        public async Task<int> DrawRevenueAndProfitDiagram(string choose, DateTime startDate, DateTime endDate)
+        {
+            if(choose == "From date to date")
+            {
+                var revenue = await StatisticServices.GetRevenueInDateRangeAsync(startDate, endDate);
+            } else if (choose == "By month")
+            {
+                var revenue = await StatisticServices.GetRevenueByMonthAsync();
+            } else
+            {
+                var revenue = await StatisticServices.GetRevenueByYearAsync();
+            }
+            return 0;
+        }
+    }
+}
