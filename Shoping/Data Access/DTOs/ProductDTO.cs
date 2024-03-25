db1edc44a47303e3d211bbd6d969344280de84cd
hoangbuu313 <buu.nguyen@gosmartlog.com>
2024-03-25 21:27:57 +0700

    Update class Product and ProductDTO

--- Shoping/Data Access/DTOs/ProductDTO.cs
+++ Shoping/Data Access/DTOs/ProductDTO.cs
@@ -12,7 +12,7 @@ namespace Shoping.Data_Access.DTOs
         public Guid RecID { get; set; }
         public Guid ProductID { get; set; }
         public string Name { get; set; }
-        public int Price { get; set; }
+        public decimal Price { get; set; }
         public string Image { get; set; } // Implement later
         public object Clone()
         {
